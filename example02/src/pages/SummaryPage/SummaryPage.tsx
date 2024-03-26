3a1b930ed9d7b320301ada7ce68ff89d7860af41
Rachel <borapark13@gmail.com>
2024-03-26 19:48:45 +0900

    #feat test update.

--- example02/src/pages/SummaryPage/SummaryPage.tsx
+++ example02/src/pages/SummaryPage/SummaryPage.tsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SummaryPage() {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <></>
-    // <form>
-    //   <h2>주문 확인</h2>
-    //   <div>
-    //     <p>
-    //       Products: <span>₩1,000</span>
-    //     </p>
-    //     <ol>
-    //       <li>
-    //         <span>Product 1</span>
-    //       </li>
-    //     </ol>
-    //   </div>
-    //   <input type="checkbox" id="confirm-order" />
-    //   <label htmlFor="confirm-order">주문하려는 것을 확인하셨나요?</label>
-    //   <button type="submit">주문 확인</button>
-    // </form>
+    <form>
+      <h2>주문 확인</h2>
+      <div>
+        <p>
+          Products: <span>₩1,000</span>
+        </p>
+        <ol>
+          <li>
+            <span>Product 1</span>
+          </li>
+        </ol>
+      </div>
+      <input
+        type="checkbox"
+        id="confirm-order"
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+      <label htmlFor="confirm-order">주문하려는 것을 확인하셨나요?</label>
+      <button type="submit" disabled={!checked}>
+        주문 확인
+      </button>
+    </form>
   );
 }
 
--- example02/src/pages/SummaryPage/tests/SummaryPage.test.tsx
+++ example02/src/pages/SummaryPage/tests/SummaryPage.test.tsx
@@ -1,19 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import SummaryPage from "../SummaryPage";
-import userEvent from "@testing-library/user-event";
 
-test("주문 확인 체크 박스를 눌러야만 주문 확인 버튼을 누를 수 있다.", () => {
+test("주문 확인 체크 박스를 눌러야만 주문 확인 버튼을 누를 수 있다.", async () => {
   render(<SummaryPage />);
 
-  const checkBoxElement = screen.getByRole("checkbox");
-  const submitButtonElement = screen.getByRole("button", { name: /submit/i });
+  const checkBoxElement = screen.getByRole("checkbox", {
+    name: "주문하려는 것을 확인하셨나요?",
+  }) as HTMLInputElement;
+  const submitButtonElement = screen.getByRole("button", {
+    name: "주문 확인",
+  }) as HTMLButtonElement;
 
-  expect(submitButtonElement).toBeDisabled();
-  userEvent.click(checkBoxElement);
-
-  if (checkBoxElement.getAttribute("checked")) {
-    userEvent.click(submitButtonElement);
-  }
-
-  expect(submitButtonElement).toBeEnabled();
+  expect(checkBoxElement.checked).toEqual(false);
+  expect(submitButtonElement.disabled).toBeTruthy();
 });
